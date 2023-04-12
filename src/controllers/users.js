5c1014ea54e56000b8909173d466caeb50b80091
Ali Haqiqi <aliz@foresightanalytics.ca>
2023-04-12 16:25:26 +0330

    sync recorded days query with new schema and add new endpoint for adding recorded day
    Signed-off-by: Ali Haqiqi <aliz@foresightanalytics.ca>

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,12 +1,23 @@
 import { recordedDays as recordedDaysService } from '../services/index.js';
 
 export const getRecordedDaysForUser = async (req, res) => {
-    const r = await recordedDaysService.getRecordedDaysForUser(
-        req.params.id,
-    );
-    return res.sendStatus(200);
+    const r = await recordedDaysService
+      .getRecordedDaysForUser(req.params.id);
+    return res.send(r);
+};
+
+export const addRecordedDayForUser = async (req, res) => {
+  const context = {
+    user_id: req.params.id,
+    // deconstruct body for whole data of recorded day
+  };
+  const r = await recordedDaysService
+    .addRecordedDayForUser(context);
+  if (!r) return res.sendStatus(400);
+  return res.sendStatus(200);
 };
 
 export default {
   getRecordedDaysForUser,
+  addRecordedDayForUser,
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { users as usersController } from '../controllers/index.js';
 const router = express.Router();
 
+router.post('/:id/recorded-day', usersController.addRecordedDayForUser);
 router.get('/:id/recorded-days', usersController.getRecordedDaysForUser);
 
 export default router;
