909e9add4f9ca557b7b64cd715713b067d99e07c
Ali Haqiqi <aliz@foresightanalytics.ca>
2023-09-28 17:36:52 +0330

    update port on prod
    Signed-off-by: Ali Haqiqi <aliz@foresightanalytics.ca>

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,7 @@ const productsRouter = require('./products');
 
 const init = () => {
   const app = express();
-  const port = 8081;
+  const port = process.env.NODE_ENV === 'production' ? 80 : 8081;
 
   app.use(cors());
 
