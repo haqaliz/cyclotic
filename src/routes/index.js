2ac56ad35c064a9fb84e1544c7bf2921bef77198
Ali Haqiqi <aliz@foresightanalytics.ca>
2023-10-04 13:08:23 +0330

    add nodemailer and test route
    Signed-off-by: Ali Haqiqi <aliz@foresightanalytics.ca>

--- src/routes/index.js
+++ src/routes/index.js
@@ -26,6 +26,25 @@ const init = () => {
   app.use('/user', userRouter);
   app.use('/products', productsRouter);
 
+  app.get('/test', async (req, res) => {
+    const nodemailer = require('nodemailer');
+    const transport = nodemailer.createTransport({
+      host: "mail.cyclo.dev",
+      port: 465,
+      auth: {
+        user: "noreply@cyclo.dev",
+        pass: "king_1374"
+      }
+    });
+    const mailOptions = {
+      to: 'aliz@foresightanalytics.com',
+      subject: 'heLLLLo',
+      html: '<strong style="background-color: red; padding: 1rem; border-radius: 5px;">Heyyyyy</strong>',
+    }
+    await transport.sendMail(mailOptions);
+    return res.sendStatus(200)
+  })
+
   app.listen(port, () => {
     console.log(`app listening on port ${port}`);
   });
