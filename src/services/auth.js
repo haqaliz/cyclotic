da92a0519cfe77423bff2c673ff233bf3fdbf07f
Ali Haqiqi <aliz@foresightanalytics.ca>
2023-11-01 16:55:42 +0330

    decode token dynamically
    Signed-off-by: Ali Haqiqi <aliz@foresightanalytics.ca>

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,16 @@
 var jwt = require('jsonwebtoken');
 const verifyToken = async (idToken) => {
-    return jwt.verify(idToken, process.env.FIREBASE_TOKEN_SECRET);
+    const r = await fetch(process.env.GSERVICE_SECURE_TOKEN_API).catch((e) => e.response);
+    if (!r?.ok) throw new Error('secured tokens couldn\'t fetch');
+    const GST = await r.json();
+    try {
+        for (const i in GST) {
+            const secret = GST[i];
+            return jwt.verify(idToken, secret);
+        }
+    } catch (e) {
+        // invalid signature
+    }
 };
 
 module.exports = {
